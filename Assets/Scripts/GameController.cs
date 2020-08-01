97308ad56b4eebc9d37c76bf402b496a8c36e132
heath <roush.heath@gmail.com>
2020-08-01 14:50:42 -0400

    Finish json database update.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,25 +1,23 @@
-﻿using System.Collections;
+﻿using System.IO;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using SimpleJSON;
 
 public class GameController : MonoBehaviour
 {
 
     public GameObject barnTab;
+    public TextAsset defaultData;
 
-    Dictionary<string, float> playerResources = new Dictionary<string, float>()
-    {
-        {"clout", 10},
-        {"horse meat", 0},
-        {"acreage", 1},
-    };
-
+    private JSONNode jsonNode;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerDbCheckandLoad();
     }
 
     // Update is called once per frame
@@ -30,11 +28,31 @@ public class GameController : MonoBehaviour
 
     public float GetResource(string resourceName)
     {
-        return playerResources[resourceName];
+        return jsonNode["Resources"][resourceName]["value"];
     }
 
     public void SetResource(string resourceName, float newValue)
     {
-        playerResources[resourceName] = newValue;
+        jsonNode["Resources"][resourceName]["value"] = newValue;
     }
+
+private void playerDbCheckandLoad()
+{
+    var fileName = Path.Combine(Application.persistentDataPath, "BADHdb.json");
+    if(!File.Exists(fileName))
+    {
+        Debug.Log("player db does not exist");
+
+        StreamWriter writer = new StreamWriter(fileName, true);
+        writer.WriteLine(defaultData.text);
+        writer.Close();
+    }
+        Debug.Log("Load data");
+
+        StreamReader reader = new StreamReader(fileName); 
+        var playerdata = reader.ReadToEnd();
+        jsonNode = SimpleJSON.JSON.Parse(playerdata);
+        reader.Close();
+        Debug.Log(jsonNode);   
+}
 }
