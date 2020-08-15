3b253de3ece627876eb81248bef1e5aab4e90c55
heath <roush.heath@gmail.com>
2020-08-14 22:04:54 -0400

    Allow for changes to default db to merge with player db. This does not remove unneeded variables from player db or update changed defaults. Also change name of gitignore.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,7 +10,7 @@ public class GameController : MonoBehaviour
 {
 
     public GameObject barnTab;
-    public TextAsset defaultData;
+    public TextAsset defaultDataFile;
 
     private JSONNode jsonNode;
 
@@ -44,15 +44,32 @@ private void playerDbCheckandLoad()
         Debug.Log("player db does not exist");
 
         StreamWriter writer = new StreamWriter(fileName, true);
-        writer.WriteLine(defaultData.text);
+        writer.WriteLine(defaultDataFile.text);
         writer.Close();
     }
         Debug.Log("Load data");
 
-        StreamReader reader = new StreamReader(fileName); 
-        var playerdata = reader.ReadToEnd();
+        StreamReader playerReader = new StreamReader(fileName); 
+        var playerdata = playerReader.ReadToEnd();
         jsonNode = SimpleJSON.JSON.Parse(playerdata);
-        reader.Close();
-        Debug.Log(jsonNode);   
+        playerReader.Close();
+
+        var defaultJsonNode = SimpleJSON.JSON.Parse(defaultDataFile.text);
+        jsonNode = mergeDefaultData(defaultJsonNode, jsonNode);
+        Debug.Log(jsonNode);  
+}
+
+public static JSONNode mergeDefaultData(JSONNode defaultJson, JSONNode playerJson) 
+{
+
+    foreach(var node in defaultJson) {
+        if(!playerJson.HasKey(node.Key)){
+            playerJson.Add(node.Key, node.Value);
+        }else{
+            mergeDefaultData(node.Value, playerJson[node.Key]); 
+        }
+    }
+
+    return playerJson;
 }
 }
